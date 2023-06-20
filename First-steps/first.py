0cb6eaf396e4765a13cbe6b1bdde70c3eaf575b6
Nazso <zsolt.nagy.1976@gmail.com>
2023-06-20 16:40:47 +0200

    learn py

--- First-steps/first.py
+++ First-steps/first.py
@@ -2,3 +2,30 @@ print('Hello world!')
 
 if 5 > 2:
     print("Five is greater than two!")
+
+#This is just a test of comment
+
+"""
+Multiline comment
+"""
+
+x = 5
+y = 'Try to learn Python'
+
+print(x, y)
+print(type(x))
+
+x = "Öt"
+print(x)
+print(type(x))
+
+a = str(4)
+b = int(4)
+c = float(4)
+
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+print(c)
+print(type(c))
