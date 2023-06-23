66ee9fabe0e1a982d44818d90b6260ee7fbe480d
Nazso <zsolt.nagy.1976@gmail.com>
2023-06-23 21:38:56 +0200

    learn py bool

---
+++ First-steps/booleans.py
@@ -0,0 +1,39 @@
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+print('-------------------------')
+
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+print('-------------------------')
+
+print(bool("Hello"))
+print(bool(15))
+
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+xx = ""
+yy = 0
+
+print(bool(xx))
+print(bool(yy))
+
+print(bool(["apple", "cherry", "banana"]))
+print(bool([]))
+
+print('-------------------------')
+print('-------------------------')
+print('-------------------------')
+print('-------------------------')
+print('-------------------------')
