e1b22a403675b8f84dc8f681603210d0a3fabf8a
Nazso <zsolt.nagy.1976@gmail.com>
2023-06-24 00:13:19 +0200

    learn py strings

--- First-steps/strings.py
+++ First-steps/strings.py
@@ -194,6 +194,35 @@ print(b.zfill(10))
 print(c.zfill(10))
 
 print('-----------------------')
+
+price = 49
+txt = "The price is {} dollars"
+print(txt.format(price))
+
+txt = "The price is {:.2f} dollars"
+print(txt.format(price))
+
 print('-----------------------')
+
+quantity = 3
+itemno = 567
+price = 49
+myorder = "I want {} pieces of item number {} for {:.2f} dollars."
+print(myorder.format(quantity, itemno, price))
+myorder = "I want {:.2f} pieces of item number {} for {:.2f} dollars."
+print(myorder.format(quantity, itemno, price))
+
 print('-----------------------')
+
+myorder = "I want {0} pieces of item number {1} for {2:.2f} dollars."
+print(myorder.format(quantity, itemno, price))
+
+age = 36
+name = "John"
+txt = "His name is {1}. {1} is {0} years old."
+print(txt.format(age, name))
+
 print('-----------------------')
+
+myorder = "I have a {carname}, it is a {model}."
+print(myorder.format(carname = "Ford", model = "Mustang"))
