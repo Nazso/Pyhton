676669139dc23e3162e4481650b6e6adcd660fd3
Nazso <zsolt.nagy.1976@gmail.com>
2023-06-23 13:06:31 +0200

    learn py strings

---
+++ First-steps/strings.py
@@ -0,0 +1,46 @@
+a = "Hello"
+print(a)
+
+print('-----------------------')
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+print('-----------------------')
+
+a = "Hello, World!"
+print(a[1])
+print(a[8])
+
+print('-----------------------')
+
+for x in "banana":
+  print(x)
+
+print('-----------------------')
+
+a = "Hello, World!"
+print(len(a))
+
+print('-----------------------')
+
+txt = "The best things in life are free!"
+print("free" in txt)
+print("best" in txt)
+print("stirng" in txt)
+
+print('-----------------------')
+
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+print('-----------------------')
+
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
+
+print('-----------------------')
