a8aaad74fddcbdf923134c4d4abffd7faca229a2
Nazso <zsolt.nagy.1976@gmail.com>
2023-06-24 15:37:57 +0200

    learn py lists

--- First-steps/lists.py
+++ First-steps/lists.py
@@ -190,6 +190,43 @@ thislist.extend(thistuple)
 print(thislist)
 
 print('-------------------------')
+
+"""
+The remove() method removes the specified item.
+"""
+
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+
+"""
+The pop() method removes the specified index.
+"""
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(0)
+print(thislist)
+
+"""
+If you do not specify the index, the pop() method removes the last item.
+"""
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()
+print(thislist)
+
+"""
+The del keyword also removes the specified index.
+The del keyword can also delete the list completely.
+"""
+
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+del thislist # clompletely list is deleted
+
 print('-------------------------')
 print('-------------------------')
 print('-------------------------')
