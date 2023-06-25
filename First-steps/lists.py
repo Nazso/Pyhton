354f4eb5d30810b759877b669f63bfd7543cd0b2
Nazso <zsolt.nagy.1976@gmail.com>
2023-06-25 14:05:51 +0200

    learn py lists

--- First-steps/lists.py
+++ First-steps/lists.py
@@ -328,6 +328,15 @@ print(newlistTwo)
 print(newlistThree)
 
 print('-------------------------')
+
+"""
+List objects have a sort() method that will sort the list alphanumerically, ascending, by default
+"""
+
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
+
 print('-------------------------')
 print('-------------------------')
 print('-------------------------')
