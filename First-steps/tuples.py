3b7a828a5945ddf05563d4e06c1ec950111dfe13
Nazso <zsolt.nagy.1976@gmail.com>
2023-06-27 23:33:50 +0200

    learn py tuples

--- First-steps/tuples.py
+++ First-steps/tuples.py
@@ -112,6 +112,31 @@ thistupleOne += y
 print(thistupleOne)
 
 print('---------------------------')
+
+"""
+Tuples are unchangeable, so you cannot remove items from it,
+but you can use the same workaround as we used for changing and adding tuple items
+
+Convert the tuple into a list, remove "apple", and convert it back into a tuple
+"""
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.remove("apple")
+thistuple = tuple(y)
+
+print(thistuple)
+
+"""
+Or you can delete the tuple completely
+
+The del keyword can delete the tuple completely
+"""
+
+thistuple = ("apple", "banana", "cherry")
+del thistuple
+print(thistuple) #this will raise an error because the tuple no longer exists
+
 print('---------------------------')
 print('---------------------------')
 print('---------------------------')
