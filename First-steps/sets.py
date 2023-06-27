4bc2471dbbeae3dc93e5366db26172b1d8f0b903
Nazso <zsolt.nagy.1976@gmail.com>
2023-06-27 23:57:18 +0200

    learn py sets

--- First-steps/sets.py
+++ First-steps/sets.py
@@ -34,8 +34,35 @@ Sets cannot have two items with the same value.
 thisset = {"apple", "banana", "cherry"}
 print(thisset)
 
+thisset = {"apple", "banana", "cherry", "apple"}
+print(thisset)
+
+"""
+The values True and 1 are considered the same value in sets, and are treated as duplicates
+"""
+
+thisset = {"apple", "banana", "cherry", True, 1, 2}
+
+print(thisset)
 
 print("--------------------------------------")
+
+thisset = {"apple", "banana", "cherry"}
+
+print(len(thisset))
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+
+print(set1)
+print(set2)
+print(set3)
+
+setOne = {"abc", 34, True, 40, "male"}
+print(setOne)
+
+
 print("--------------------------------------")
 print("--------------------------------------")
 print("--------------------------------------")
