e4cbb4b83e8f537930572262a2c385d672b7aabd
Nazso <zsolt.nagy.1976@gmail.com>
2023-06-28 00:07:27 +0200

    learn py sets

--- First-steps/sets.py
+++ First-steps/sets.py
@@ -104,4 +104,37 @@ print("melone" in thisset)
 # Once a set is created, you cannot change its items, but you can add new items.
 
 print("--------------------------------------")
+
+"""
+Once a set is created, you cannot change its items, but you can add new items.
+
+To add one item to a set use the add() method.
+
+ExampleGet your own Python Server
+Add an item to a set, using the add() method:
+
+"""
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.add("orange")
+
+print(thisset)
+
+"""
+To add items from another set into the current set, use the update() method.
+
+Example
+Add elements from tropical into thisset:
+
+"""
+
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+
+thisset.update(tropical)
+
+print(thisset)
+
+
 print("--------------------------------------")
