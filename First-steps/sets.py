43098faddd7121719c0a50eec937f2095aebbe15
Nazso <zsolt.nagy.1976@gmail.com>
2023-06-28 00:15:18 +0200

    learn py sets

--- First-steps/sets.py
+++ First-steps/sets.py
@@ -207,4 +207,17 @@ del thisset
 # print(thisset)
 
 print("--------------------------------------")
+
+"""
+You can loop through the set items by using a for loop:
+
+ExampleGet your own Python Server
+Loop through the set, and print the values:
+"""
+
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
+
 print("--------------------------------------")
