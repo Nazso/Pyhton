830fa1cced992135a8a441b9781a6c736ea129d8
Nazso <zsolt.nagy.1976@gmail.com>
2023-06-27 23:43:57 +0200

    learn py tuples

--- First-steps/tuples.py
+++ First-steps/tuples.py
@@ -21,6 +21,14 @@ Unchangeable
 Tuples are unchangeable, meaning that we cannot change, add or remove items after the tuple has been created.
 """
 
+"""
+Python has two built-in methods that you can use on tuples.
+
+Method	Description
+count()	Returns the number of times a specified value occurs in a tuple
+index()	Searches the tuple for a specified value and returns the position of where it was found
+"""
+
 thistuple = ("apple", "banana", "cherry", "apple", "cherry")
 print(thistuple)
 
