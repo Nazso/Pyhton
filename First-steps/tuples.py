889011004d059c027429e2f3dc31fa7b81b7fe52
Nazso <zsolt.nagy.1976@gmail.com>
2023-06-27 23:37:02 +0200

    learn py tuples

--- First-steps/tuples.py
+++ First-steps/tuples.py
@@ -135,8 +135,29 @@ The del keyword can delete the tuple completely
 
 thistuple = ("apple", "banana", "cherry")
 del thistuple
-print(thistuple) #this will raise an error because the tuple no longer exists
+# print(thistuple) #this will raise an error because the tuple no longer exists
 
 print('---------------------------')
+
+fruits = ("apple", "banana", "cherry")
+
+(green, yellow, red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+"""
+The number of variables must match the number of values in the tuple,
+if not, you must use an asterisk to collect the remaining values as a list.
+"""
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+
+(green, yellow, *red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
 print('---------------------------')
 print('---------------------------')
