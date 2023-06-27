caf1642fe7d4b16dbbd5c2fb73b9e665257c64e4
Nazso <zsolt.nagy.1976@gmail.com>
2023-06-27 23:38:10 +0200

    learn py tuples

--- First-steps/tuples.py
+++ First-steps/tuples.py
@@ -159,5 +159,14 @@ print(green)
 print(yellow)
 print(red)
 
+
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+
+(green, *tropic, red) = fruits
+
+print(green)
+print(tropic)
+print(red)
+
 print('---------------------------')
 print('---------------------------')
