6fb923f1fd4a981498e9f91e9c73f35366684b3f
Nazso <zsolt.nagy.1976@gmail.com>
2023-06-28 00:12:20 +0200

    learn py sets

--- First-steps/sets.py
+++ First-steps/sets.py
@@ -168,6 +168,24 @@ thisset.discard("apple")
 
 print(thisset)
 
+# If the item to remove does not exist, discard() will NOT raise an error.
+
 print("--------------------------------------")
+
+"""
+You can also use the pop() method to remove an item,
+but this method will remove a random item,
+so you cannot be sure what item that gets removed.
+
+The return value of the pop() method is the removed item.
+"""
+
+thisset = {"apple", "banana", "cherry"}
+
+x = thisset.pop()
+
+print(x)
+print(thisset)
+
 print("--------------------------------------")
 print("--------------------------------------")
