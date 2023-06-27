35d4173003d8ca5d928eadc25d4cdf55239a53be
Nazso <zsolt.nagy.1976@gmail.com>
2023-06-27 23:59:19 +0200

    learn py sets

--- First-steps/sets.py
+++ First-steps/sets.py
@@ -64,7 +64,17 @@ print(setOne)
 
 
 print("--------------------------------------")
+
+myset = {"apple", "banana", "cherry"}
+print(type(myset))
+
 print("--------------------------------------")
+
+# Using the set() constructor to make a set:
+
+thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+print(thisset)
+
 print("--------------------------------------")
 print("--------------------------------------")
 print("--------------------------------------")
