8c2d608e3ddfe963fcf139ed66ee9fcfc6bd7498
Nazso <zsolt.nagy.1976@gmail.com>
2023-06-28 00:10:19 +0200

    learn py sets

--- First-steps/sets.py
+++ First-steps/sets.py
@@ -150,6 +150,24 @@ print(thisset)
 
 
 print("--------------------------------------")
+
+
+# To remove an item in a set, use the remove(), or the discard() method.
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.remove("banana")
+
+print(thisset)
+
+# If the item to remove does not exist, remove() will raise an error.
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.discard("apple")
+
+print(thisset)
+
 print("--------------------------------------")
 print("--------------------------------------")
 print("--------------------------------------")
