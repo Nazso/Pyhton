b4bb84307159e9aeaec1390dca9dbab0e715134e
Nazso <zsolt.nagy.1976@gmail.com>
2023-06-30 11:40:06 +0200

    learn py dict

--- First-steps/dictionaries.py
+++ First-steps/dictionaries.py
@@ -15,6 +15,21 @@ thisdict = {
 }
 print(thisdict)
 
+
+"""
+Dictionary items are ordered, changeable, and does not allow duplicates.
+
+Dictionary items are presented in key:value pairs, and can be referred to by using the key name
+"""
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
+print(thisdict["model"])
+
 print("--------------------------------------")
 print("--------------------------------------")
 print("--------------------------------------")
