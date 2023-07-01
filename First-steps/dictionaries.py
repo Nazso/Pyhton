ee9c006f23c99fdc77c4d2d0635134a3ec383d85
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-01 21:49:08 +0200

    learn py dict

--- First-steps/dictionaries.py
+++ First-steps/dictionaries.py
@@ -74,6 +74,21 @@ x = thisdictOne.keys()
 
 print(x)
 
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.keys()
+
+print(x) #before the change
+
+car["color"] = "white"
+
+print(x) #after the change
+
+
 print("--------------------------------------")
 print("--------------------------------------")
 print("--------------------------------------")
