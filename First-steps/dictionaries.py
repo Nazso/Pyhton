492f39a44f00d52ee2dfbd3fe67fc1a69a7abd14
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-02 14:29:33 +0200

    learn py dict

--- First-steps/dictionaries.py
+++ First-steps/dictionaries.py
@@ -147,3 +147,44 @@ if "model" in thisdictTwo:
   print("Yes, 'model' is one of the keys in the thisdictTwo dictionary")
 
 print("--------------------------------------")
+
+"""
+Change Values
+You can change the value of a specific item by referring to its key name:
+
+Example
+Change the "year" to 2018:
+
+"""
+
+thisdictThree = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdictThree["year"] = 2018
+print(thisdictThree)
+
+"""
+Update Dictionary
+The update() method will update the dictionary with the items from the given argument.
+
+The argument must be a dictionary, or an iterable object with key:value pairs.
+
+Example
+Update the "year" of the car by using the update() method:
+
+"""
+
+thisdictFour = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdictFour.update({"year": 2020})
+
+print(thisdictFour)
+
+print("--------------------------------------")
+print("--------------------------------------")
+print("--------------------------------------")
