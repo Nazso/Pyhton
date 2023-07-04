978b9e44c83c5521212e3c713bc0f482d41761fc
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-04 20:13:31 +0200

    learn py dict

--- First-steps/dictionaries.py
+++ First-steps/dictionaries.py
@@ -382,6 +382,32 @@ mydictTwo = dict(thisdict)
 print(mydictTwo)
 
 print("--------------------------------------")
+
+"""
+Nested Dictionaries
+A dictionary can contain dictionaries, this is called nested dictionaries.
+
+Example
+Create a dictionary that contain three dictionaries:
+"""
+
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+
+print(myfamily)
+
 print("--------------------------------------")
 print("--------------------------------------")
 print("--------------------------------------")
