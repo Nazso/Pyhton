2c5588d1bbb62773f47eb2f879dfd5e78d9bb4ad
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-05 20:36:17 +0200

    learn py conditionals

--- First-steps/loops_conditionals.py
+++ First-steps/loops_conditionals.py
@@ -27,6 +27,21 @@ Python relies on indentation (whitespace at the beginning of a line) to define s
 If statement, without indentation (will raise an error)
 """
 
+print("-------elif-----")
+
+"""
+Elif
+The elif keyword is Python's way of saying "if the previous conditions were not true, then try this condition".
+
+Example
+"""
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
 
 print("------------Loops------------")
 
