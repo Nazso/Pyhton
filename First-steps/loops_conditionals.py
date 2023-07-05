e80cb7038d13cfaad3dd33fa914cc8fe1de73827
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-05 20:38:30 +0200

    learn py

--- First-steps/loops_conditionals.py
+++ First-steps/loops_conditionals.py
@@ -47,6 +47,20 @@ In this example a is equal to b, so the first condition is not true,
 but the elif condition is true, so we print to screen that "a and b are equal".
 """
 
+print("-----Else----")
+"""
+Else
+The else keyword catches anything which isn't caught by the preceding conditions.
+Example
+"""
+a = 200
+b = 330
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
 
 print("------------Loops------------")
 
