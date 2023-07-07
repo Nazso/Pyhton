b7966c423f101d1ad4cd8632e4029166a611dd40
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-07 12:28:02 +0200

    learn py iterators

--- First-steps/iterators.py
+++ First-steps/iterators.py
@@ -24,6 +24,24 @@ print(next(myit))
 print(next(myit))
 
 print("---------------------------------------------")
+
+"""
+Even strings are iterable objects, and can return an iterator:
+
+Example
+Strings are also iterable objects, containing a sequence of characters:
+
+"""
+mystr = "banana"
+myit = iter(mystr)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
 print("---------------------------------------------")
 print("---------------------------------------------")
 print("---------------------------------------------")
