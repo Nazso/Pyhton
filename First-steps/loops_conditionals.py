14343e647ebd95aba25e671a18089b1c2e125cb7
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-07 12:11:14 +0200

    learn py for loops

--- First-steps/loops_conditionals.py
+++ First-steps/loops_conditionals.py
@@ -376,5 +376,17 @@ however it is possible to specify the starting value by adding a parameter:
 for x in range(22, 26):
   print(x)
 
+print("---")
+
+"""
+The range() function defaults to increment the sequence by 1, however it is possible to specify the increment value by adding a third parameter: range(2, 30, 3):
+
+Example
+Increment the sequence with 3 (default is 1):
+"""
+
+for x in range(2, 20, 3):
+  print(x)
+
 print("-----------------------------------------")
 print("-----------------------------------------")
