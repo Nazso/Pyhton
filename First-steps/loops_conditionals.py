cf167074f63da29aebc7d319a0230a9535493937
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-07 12:02:23 +0200

    learn py for loops

--- First-steps/loops_conditionals.py
+++ First-steps/loops_conditionals.py
@@ -302,4 +302,8 @@ fruits = ["apple", "banana", "cherry"]
 for x in fruits:
   print(x)
 
+# The for loop does not require an indexing variable to set beforehand.
+
+
+
 print("-----------------------------------------")
