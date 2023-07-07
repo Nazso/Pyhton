842153c17ece33c165f4143bcae61d27abbc0679
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-07 12:29:18 +0200

    learn py iterators

--- First-steps/iterators.py
+++ First-steps/iterators.py
@@ -43,6 +43,21 @@ print(next(myit))
 print(next(myit))
 
 print("---------------------------------------------")
+
+
+"""
+Looping Through an Iterator
+We can also use a for loop to iterate through an iterable object:
+
+Example
+Iterate the values of a tuple:
+"""
+
+mytuple = ("apple", "banana", "cherry")
+
+for x in mytuple:
+  print(x)
+
 print("---------------------------------------------")
 print("---------------------------------------------")
 print("---------------------------------------------")
