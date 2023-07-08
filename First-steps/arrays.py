4eeede9d5d3ec1609745416eb9b6ed63a12e7d51
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-08 14:35:14 +0200

    learn py arrays

--- First-steps/arrays.py
+++ First-steps/arrays.py
@@ -34,5 +34,77 @@ print(w)
 
 
 print("-------------------------")
+
+# Modify the value of the first array item:
+
+cars[0] = "Toyota"
+print(cars)
+
 print("-------------------------")
+
+"""
+The Length of an Array
+Use the len() method to return the length of an array (the number of elements in an array).
+
+Example
+Return the number of elements in the cars array:
+"""
+
+length = len(cars)
+
+print(length)
+
+# Note: The length of an array is always one more than the highest array index.
+
+print("-------------------------")
+
+"""
+Looping Array Elements
+You can use the for in loop to loop through all the elements of an array.
+
+Example
+Print each item in the cars array:
+"""
+
+for x in cars:
+  print(x)
+
+print("-------------------------")
+
+"""
+Adding Array Elements
+You can use the append() method to add an element to an array.
+
+Example
+Add one more element to the cars array:
+"""
+
+cars.append("Honda")
+print(cars)
+
+print("-------------------------")
+
+"""
+Removing Array Elements
+You can use the pop() method to remove an element from the array.
+
+Example
+Delete the second element of the cars array:
+"""
+
+cars.pop(1)
+print(cars)
+
+"""
+You can also use the remove() method to remove an element from the array.
+
+Example
+Delete the element that has the value "Volvo":
+"""
+cars.remove("BMW")
+print(cars)
+
+
+# Note: The list's remove() method only removes the first occurrence of the specified value.
+
 print("-------------------------")
