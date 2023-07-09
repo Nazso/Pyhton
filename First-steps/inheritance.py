d9ad9c5f17da12036e67170904bb0e28dde17159
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-09 11:34:44 +0200

    learn py inheritance

--- First-steps/inheritance.py
+++ First-steps/inheritance.py
@@ -28,6 +28,25 @@ x.printname()
 
 
 print("--------------------------------------------")
+
+"""
+Create a Child Class
+To create a class that inherits the functionality from another class, send the parent class as a parameter when creating the child class:
+
+Example
+Create a class named Student, which will inherit the properties and methods from the Person class:
+"""
+
+class Student(Person):
+  pass
+
+# Note: Use the pass keyword when you do not want to add any other properties or methods to the class.
+
+# Use the Student class to create an object, and then execute the printname method:
+
+y = Student("Mike", "Olsen")
+y.printname()
+
 print("--------------------------------------------")
 print("--------------------------------------------")
 print("--------------------------------------------")
