41b4b28c3a774c504bc14dcb69503531b17bc0d2
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-09 12:03:06 +0200

    learn py inheritance

--- First-steps/inheritance.py
+++ First-steps/inheritance.py
@@ -196,5 +196,26 @@ print(x.graduationyear)
 
 
 print("--------------------------------------------")
+
+"""
+Add Methods
+Example
+Add a method called welcome to the Student class:
+"""
+
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+  def welcome(self):
+    print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
+
+xz = Student("Joe", "Svensson", 2015)
+xz.welcome()
+
+# If you add a method in the child class with the same name as a function in the parent class,
+# the inheritance of the parent method will be overridden.
+
 print("--------------------------------------------")
 print("--------------------------------------------")
