8f0093e362e06394145b00cb4e379154d8c549e9
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-10 20:23:22 +0200

    learn py scope

--- First-steps/scope.py
+++ First-steps/scope.py
@@ -55,4 +55,61 @@ myfunc() # variable is available inside of the functions
 print(x) # variabla is available everwhere inside the code(global scope)
 
 print("----------------------------------------")
+
+"""
+Naming Variables
+If you operate with the same variable name inside and outside of a function,
+Python will treat them as two separate variables, one available in the global scope (outside the function)
+and one available in the local scope (inside the function):
+
+Example
+The function will print the local x, and then the code will print the global x:
+"""
+
+x = 150
+
+def myfunc():
+  x = 280
+  print(x)
+
+myfunc()
+
+print(x)
+
 print("----------------------------------------")
+
+"""
+Global Keyword
+If you need to create a global variable, but are stuck in the local scope, you can use the global keyword.
+
+The global keyword makes the variable global.
+
+Example
+If you use the global keyword, the variable belongs to the global scope:
+"""
+
+def myfunc():
+  global x
+  x = 750
+
+myfunc()
+
+print(x)
+
+print("----")
+"""
+Also, use the global keyword if you want to make a change to a global variable inside a function.
+
+Example
+To change the value of a global variable inside a function, refer to the variable by using the global keyword:
+"""
+
+x = 600
+
+def myfunc():
+  global x
+  x = 550
+
+myfunc()
+
+print(x)
