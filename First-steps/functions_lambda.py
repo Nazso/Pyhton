75c2905b8afc5c5d5f9450af0a1eff212299396c
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-11 16:57:11 +0200

    learn py func

--- First-steps/functions_lambda.py
+++ First-steps/functions_lambda.py
@@ -69,5 +69,21 @@ my_function("Glenn", "Quagmire")
 # If you try to call the function with 1 or 3 arguments, you will get an error:
 
 print ("-------------------------------")
+
+"""
+Arbitrary Arguments, *args
+If you do not know how many arguments that will be passed into your function, add a * before the parameter name in the function definition.
+
+This way the function will receive a tuple of arguments, and can access the items accordingly:
+
+Example
+If the number of arguments is unknown, add a * before the parameter name:
+"""
+
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+my_function("Chris", "Meg", "Stewie")
+
 print ("-------------------------------")
 print ("-------------------------------")
