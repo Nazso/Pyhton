8457d32e86af584c296c31c797ff57c231bb7bef
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-11 16:57:51 +0200

    learn py func

--- First-steps/functions_lambda.py
+++ First-steps/functions_lambda.py
@@ -72,12 +72,13 @@ print ("-------------------------------")
 
 """
 Arbitrary Arguments, *args
-If you do not know how many arguments that will be passed into your function, add a * before the parameter name in the function definition.
+If you do not know how many arguments that will be passed into your function,
+add a * before the parameter name in the function definition.
 
-This way the function will receive a tuple of arguments, and can access the items accordingly:
+This way the function will receive a tuple of arguments, and can access the items accordingly
 
 Example
-If the number of arguments is unknown, add a * before the parameter name:
+If the number of arguments is unknown, add a * before the parameter name
 """
 
 def my_function(*kids):
