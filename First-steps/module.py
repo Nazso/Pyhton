34010a573d28e85dc34bcd35e07d9769a478bd60
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-13 22:18:23 +0200

    learn py module

--- First-steps/module.py
+++ First-steps/module.py
@@ -124,3 +124,8 @@ Example
 The module named mymodule has one function and one dictionary:
 """
 
+# Import only the person1 dictionary from the module:
+
+from mymodule import person1
+
+print (person1["age"])
