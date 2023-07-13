c61bd0e650d1cf873a1f106670adb674fc5400fa
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-13 22:14:22 +0200

    learn py module

--- First-steps/module.py
+++ First-steps/module.py
@@ -59,6 +59,29 @@ print(b)
 print(c)
 
 print("--------------------------------------------")
+
+"""
+Naming a Module
+You can name the module file whatever you like, but it must have the file extension .py
+
+Re-naming a Module
+You can create an alias when you import a module, by using the as keyword:
+
+Example
+Create an alias for mymodule called mx:
+
+"""
+
+import mymodule as mx
+
+a = mx.person2["age"]
+b = mx.person2["name"]
+c = mx.person2["country"]
+
+print(a)
+print(b)
+print(c)
+
 print("--------------------------------------------")
 print("--------------------------------------------")
 print("--------------------------------------------")
