c14c0947b97172d94e038a0282744bb2641a9e2a
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-15 09:28:23 +0200

    learn py modules

---
+++ First-steps/Miscellaneous/mymodule.py
@@ -0,0 +1,16 @@
+
+def greeting(name):
+  print("Good morning, " + name + "!")
+
+
+person1 = {
+  "name": "John",
+  "age": 36,
+  "country": "Norway"
+}
+
+person2 = {
+  "name": "Peter",
+  "age": 42,
+  "country": "USA"
+}
