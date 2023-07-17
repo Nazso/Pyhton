2992cd19aab0a73b12699ca8db05f8e9329eb96b
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-17 11:28:35 +0200

    learn py math

---
+++ First-steps/Miscellaneous/math.py
@@ -0,0 +1,33 @@
+"""
+Python Math
+Python has a set of built-in math functions, including an extensive math module, that allows you to perform mathematical tasks on numbers.
+
+Built-in Math Functions
+The min() and max() functions can be used to find the lowest or highest value in an iterable:
+
+ExampleGet your own Python Server
+"""
+
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+print("---------------------------------------")
+
+"""
+The abs() function returns the absolute (positive) value of the specified number:
+
+Example
+"""
+
+x = abs(-7.25)
+
+print(x)
+
+print("---------------------------------------")
+print("---------------------------------------")
+print("---------------------------------------")
+print("---------------------------------------")
+print("---------------------------------------")
