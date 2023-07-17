9a264d85838f9ea4e4b2826063ec443b983247c5
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-17 11:19:19 +0200

    learn py dates

--- First-steps/Miscellaneous/dates.py
+++ First-steps/Miscellaneous/dates.py
@@ -10,3 +10,35 @@ import datetime
 
 x = datetime.datetime.now()
 print(x)
+
+print("-----------------------------------------")
+
+"""
+Date Output
+When we execute the code from the example above the result will be:
+
+2023-07-17 11:15:59.373072
+The date contains year, month, day, hour, minute, second, and microsecond.
+
+The datetime module has many methods to return information about the date object.
+
+Here are a few examples, you will learn more about them later in this chapter:
+
+Example
+Return the year and name of weekday:
+"""
+
+import datetime
+
+x = datetime.datetime.now()
+
+print(x.year)
+print(x.month)
+print(x.strftime("%A"))
+
+print("-----------------------------------------")
+print("-----------------------------------------")
+print("-----------------------------------------")
+print("-----------------------------------------")
+print("-----------------------------------------")
+print("-----------------------------------------")
