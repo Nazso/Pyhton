b66431946d450a9ffaf6b3a8ca09a6fa603a9dd6
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-19 11:48:26 +0200

    learn py json

--- First-steps/Miscellaneous/json_pract.py
+++ First-steps/Miscellaneous/json_pract.py
@@ -33,6 +33,28 @@ print(y["name"])
 print(y["age"])
 
 print("---------------------------------------------")
+
+"""
+Convert from Python to JSON
+If you have a Python object, you can convert it into a JSON string by using the json.dumps() method.
+
+Example
+Convert from Python to JSON:
+"""
+
+# a Python object (dict):
+xx = {
+  "name": "John",
+  "age": 30,
+  "city": "New York"
+}
+
+# convert into JSON:
+yy = json.dumps(xx)
+
+# the result is a JSON string:
+print(yy)
+
 print("---------------------------------------------")
 print("---------------------------------------------")
 print("---------------------------------------------")
