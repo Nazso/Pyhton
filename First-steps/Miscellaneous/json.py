e5126f4c132ffafcd2961c3399d7d02d470fda82
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-19 11:45:32 +0200

    learn py json

--- First-steps/Miscellaneous/json.py
+++ First-steps/Miscellaneous/json.py
@@ -8,6 +8,29 @@ JSON in Python
 Python has a built-in package called json, which can be used to work with JSON data.
 """
 
+# Import the json module:
+
+import json
+
+"""
+Parse JSON - Convert from JSON to Python
+If you have a JSON string, you can parse it by using the json.loads() method.
+
+The result will be a Python dictionary.
+
+Example
+Convert from JSON to Python:
+"""
+
+# some JSON:
+x =  '{ "name":"Luke", "age":18, "city":"Tatuin"}'
+
+# parse x:
+y = json.loads(x)
+
+# the result is a Python dictionary:
+print(y["name"])
+print(y["age"])
 
 print("---------------------------------------------")
 print("---------------------------------------------")
