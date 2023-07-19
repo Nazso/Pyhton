3a34f9bb17ad7ee7e2d1a1043eb58b2bb0a4788f
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-19 11:38:54 +0200

    learn py json

---
+++ First-steps/Miscellaneous/json.py
@@ -0,0 +1,16 @@
+"""
+Python JSON
+JSON is a syntax for storing and exchanging data.
+
+JSON is text, written with JavaScript object notation.
+
+JSON in Python
+Python has a built-in package called json, which can be used to work with JSON data.
+"""
+
+
+print("---------------------------------------------")
+print("---------------------------------------------")
+print("---------------------------------------------")
+print("---------------------------------------------")
+print("---------------------------------------------")
