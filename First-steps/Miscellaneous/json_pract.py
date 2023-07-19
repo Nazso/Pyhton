88640be24ddfe26a5cb838ab703eadf91ffb858c
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-19 11:53:34 +0200

    learn py json

--- First-steps/Miscellaneous/json_pract.py
+++ First-steps/Miscellaneous/json_pract.py
@@ -106,6 +106,19 @@ x = {
 print(json.dumps(x))
 
 print("---------------------------------------------")
+
+"""
+Format the Result
+The example above prints a JSON string, but it is not very easy to read, with no indentations and line breaks.
+
+The json.dumps() method has parameters to make it easier to read the result:
+
+Example
+Use the indent parameter to define the numbers of indents:
+"""
+
+print(json.dumps(x, indent=4))
+
 print("---------------------------------------------")
 
 """
