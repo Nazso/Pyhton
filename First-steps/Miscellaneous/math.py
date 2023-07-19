aa9ab771c14a098adeb6ec5ba28a264f62a9bc9c
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-19 11:35:04 +0200

    learn py math

--- First-steps/Miscellaneous/math.py
+++ First-steps/Miscellaneous/math.py
@@ -46,7 +46,24 @@ print(z)
 
 print("---------------------------------------")
 
+"""
+The Math Module
+Python has also a built-in module called math, which extends the list of mathematical functions.
 
+To use it, you must import the math module:
+
+import math
+When you have imported the math module, you can start using methods and constants of the module.
+
+The math.sqrt() method for example, returns the square root of a number:
+
+Example
+"""
+import math
+
+x = math.sqrt(64)
+
+print(x)
 
 print("---------------------------------------")
 print("---------------------------------------")
