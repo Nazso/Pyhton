fe0f1478d81d852d85836ca4ea4c9c204b89a4d0
Nagy Zsolt <zsolt.nagy.1976@gmail.com>
2023-08-14 13:32:31 +0200

    learn py regex

--- First-steps/Miscellaneous/regex.py
+++ First-steps/Miscellaneous/regex.py
@@ -121,6 +121,18 @@ x = re.findall("he.*o", txt)
 print(x)
 
 print("-----")
+
+# txt = "hello planet"
+# txt = "hellalko planet"
+# txt = "hellalkop planet"
+txt = "haellalko planet"
+
+#Search for a sequence that starts with "he", followed by 1 or more  (any) characters, and an "o":
+
+x = re.findall("he.+o", txt)
+
+print(x)
+
 print("-----")
 print("-----")
 
