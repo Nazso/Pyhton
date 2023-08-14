a4bff3c85a7113811277d050a3f851a982f750ae
Nagy Zsolt <zsolt.nagy.1976@gmail.com>
2023-08-14 15:30:03 +0200

    learn py regex

--- First-steps/Miscellaneous/regex.py
+++ First-steps/Miscellaneous/regex.py
@@ -331,6 +331,41 @@ print(x)
 
 
 print("---------------------------------------------------")
+
+"""
+Match Object
+A Match Object is an object containing information about the search and the result.
+
+Note: If there is no match, the value None will be returned, instead of the Match Object.
+
+Example
+Do a search that will return a Match Object:
+"""
+
+txt = "The rain in Spain"
+x = re.search("ai", txt)
+print(x)
+
+#this will print an object
+
+
+"""
+The Match object has properties and methods used to retrieve information about the search, and the result:
+
+.span() returns a tuple containing the start-, and end positions of the match.
+.string returns the string passed into the function
+.group() returns the part of the string where there was a match
+Example
+Print the position (start- and end-position) of the first match occurrence.
+
+The regular expression looks for any words that starts with an upper case "S":
+"""
+
+txt = "The rain in Spain"
+x = re.search(r"\bS\w+", txt)
+print(x.span())
+print(x.group())
+
 print("---------------------------------------------------")
 print("---------------------------------------------------")
 print("---------------------------------------------------")
