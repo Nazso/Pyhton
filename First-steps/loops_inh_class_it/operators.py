42456d411ddd182ee767b715e78e02214d62dd1c
Nagy Zsolt <zsolt.nagy.1976@gmail.com>
2023-08-14 15:51:13 +0200

    learn py pip

--- First-steps/loops_inh_class_it/operators.py
+++ First-steps/loops_inh_class_it/operators.py
@@ -201,3 +201,12 @@ print('----------------------------')
 print((6 + 3) - (6 + 3))
 print(100 + 5 * 3)
 print(5 + 4 - 7 + 3)
+
+
+import camelcase
+
+c = camelcase.CamelCase()
+
+txt = "hello world"
+
+print(c.hump(txt))
