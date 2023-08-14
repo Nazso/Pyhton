6fe39e1eb49db0e45710ede99ab43ce349138a7d
Nagy Zsolt <zsolt.nagy.1976@gmail.com>
2023-08-14 13:15:51 +0200

    learn py regex

---
+++ First-steps/Miscellaneous/regex.py
@@ -0,0 +1,22 @@
+"""
+A RegEx, or Regular Expression, is a sequence of characters that forms a search pattern.
+
+RegEx can be used to check if a string contains the specified search pattern.
+
+RegEx Module
+Python has a built-in package called re, which can be used to work with Regular Expressions.
+
+Import the re module:
+
+"""
+import re
+
+# txt = "The rain in Spain"
+txt = "alfa, beta gamma"
+x = re.search("^The.*Spain$", txt)
+
+if x:
+  print("YES! We have a match!")
+else:
+  print("No match")
+
