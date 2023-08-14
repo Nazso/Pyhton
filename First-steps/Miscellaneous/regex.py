e3102c4a5be3ea14dd0e76960503c928d81ffb86
Nagy Zsolt <zsolt.nagy.1976@gmail.com>
2023-08-14 13:17:14 +0200

    learn py regex

--- First-steps/Miscellaneous/regex.py
+++ First-steps/Miscellaneous/regex.py
@@ -21,6 +21,18 @@ else:
   print("No match")
 
 print("---------------------------------------------------")
+
+"""
+RegEx Functions
+The re module offers a set of functions that allows us to search a string for a match:
+
+    Function	Description
+    findall	    Returns a list containing all matches
+    search	    Returns a Match object if there is a match anywhere in the string
+    split	    Returns a list where the string has been split at each match
+    sub	        Replaces one or many matches with a string
+"""
+
 print("---------------------------------------------------")
 print("---------------------------------------------------")
 print("---------------------------------------------------")
