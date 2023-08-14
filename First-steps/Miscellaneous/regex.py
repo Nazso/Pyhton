d17bd0f7644d05212011d8386cdaa6498a52bf59
Nagy Zsolt <zsolt.nagy.1976@gmail.com>
2023-08-14 13:54:54 +0200

    learn py regex

--- First-steps/Miscellaneous/regex.py
+++ First-steps/Miscellaneous/regex.py
@@ -233,7 +233,38 @@ x = re.findall("ai", txt)
 print(x)
 print(len(x))
 
+"""
+The list contains the matches in the order they are found.
+
+If no matches are found, an empty list is returned:
+
+Example
+Return an empty list if no match was found:
+"""
+
+txt = "The rain in Spain"
+x = re.findall("Portugal", txt)
+print(x)
+
 print("---------------------------------------------------")
+
+"""
+The search() Function
+The search() function searches the string for a match, and returns a Match object if there is a match.
+
+If there is more than one match, only the first occurrence of the match will be returned:
+
+Example
+Search for the first white-space character in the string:
+"""
+
+# txt = "The rain in Spain"
+# txt = "Thera in in Spain"
+txt = "Therainin Spain"
+x = re.search("\s", txt)
+
+print("The first white-space character is located in position:", x.start())
+
 print("---------------------------------------------------")
 print("---------------------------------------------------")
 print("---------------------------------------------------")
