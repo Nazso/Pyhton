5cb95bcf93fd06d256600db8cfa8a26c7e3df3cd
Nagy Zsolt <zsolt.nagy.1976@gmail.com>
2023-08-25 12:07:28 +0200

    learn py indexed name

--- First-steps/Miscellaneous/user_input.py
+++ First-steps/Miscellaneous/user_input.py
@@ -85,7 +85,8 @@ Example
 quantity = 3
 itemno = 567
 price = 49
-myorder = "I want {0} pieces of item number {1} for {2:.2f} dollars."
+# myorder = "I want {0} pieces of item number {1} for {2:.2f} dollars."
+myorder = "I want {1} pieces of item number {2} for {0:.2f} dollars."
 print(myorder.format(quantity, itemno, price))
 
 """
