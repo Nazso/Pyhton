6fd957b6540ec07d702650aa40a97ea086ed3463
Nagy Zsolt <zsolt.nagy.1976@gmail.com>
2023-08-25 11:57:52 +0200

    learn py input format()

--- First-steps/Miscellaneous/user_input.py
+++ First-steps/Miscellaneous/user_input.py
@@ -16,10 +16,27 @@ Python 3.6
 """
 
 username = input("Enter username:")
-print("Username is: " + username)
+#print("Username is: " + username)
 
 # Python stops executing when it comes to the input() function, and continues when the user has given some input.
 
+print("-------------------------------string format()--------------------------------")
 
 # String formatting
+"""
+To make sure a string will display as expected, we can format the result with the format() method.
 
+String format()
+The format() method allows you to format selected parts of a string.
+
+Sometimes there are parts of a text that you do not control, maybe they come from a database, or user input?
+
+To control such values, add placeholders (curly brackets {}) in the text, and run the values through the format() method:
+
+ExampleGet your own Python Server
+Add a placeholder where you want to display the price:
+"""
+
+price = 49
+txt = "The price is {} dollars"
+print(txt.format(price))
