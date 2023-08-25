4adfb3da72f31bd710fbe2a8d164ce640dc5b771
Nagy Zsolt <zsolt.nagy.1976@gmail.com>
2023-08-25 12:29:00 +0200

    kearn py file handling

--- First-steps/file_handling/file_handling.py
+++ First-steps/file_handling/file_handling.py
@@ -38,6 +38,88 @@ Because "r" for read, and "t" for text are the default values, you do not need t
 
 Note: Make sure the file exists, or else you will get an error.
 """
+# f = open("demofile.txt")
+# f = open("demofile.txt", "rt")
+
+
+"""
+Open a File on the Server
+Assume we have the following file, located in the same folder as Python:
+
+To open the file, use the built-in open() function.
+
+The open() function returns a file object, which has a read() method for reading the content of the file:
+"""
+
+f = open("demofile.txt", "r")
+print(f.read())
+
+print("--------------------------Read Only Parts of the File---------------------------------")
+
+"""
+If the file is located in a different location, you will have to specify the file path, like this:
+
+Example
+Open a file on a different location:
+
+f = open("D:\\myfiles\welcome.txt", "r")
+print(f.read())
+"""
+
+"""
+Read Only Parts of the File
+By default the read() method returns the whole text, but you can also specify how many characters you want to return:
+
+Example
+Return the 5 first characters of the file:
+"""
+
+# f = open("demofile.txt", "r")
 f = open("demofile.txt")
-f = open("demofile.txt", "rt")
+# What is the deifference with or without "r"????
+
+print(f.read(5))
+print(f.read(13))
+print(f.read(30))
+
+print("----------------------------Read Lines-------------------------------")
+
+"""
+Read Lines
+You can return one line by using the readline() method:
+
+Example
+Read one line of the file:
+"""
+
+
+f = open("demofile.txt", "r")
+print(f.readline())
+
+print("-----------------")
+
+"""
+By calling readline() two times, you can read the two first lines:
+
+Example
+Read two lines of the file:
+"""
+
+f = open("demofile.txt", "r")
+print(f.readline())
+print(f.readline())
+
+print("-----------------")
+
+"""
+By looping through the lines of the file, you can read the whole file, line by line:
+
+Example
+Loop through the file line by line:
+"""
+
+f = open("demofile.txt", "r")
+for x in f:
+  print(x)
+
 
