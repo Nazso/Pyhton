98e076b3becd777c410baa02b12f4998a6f56ff1
Nagy Zsolt <zsolt.nagy.1976@gmail.com>
2023-08-27 17:22:08 +0200

    learn py

--- First-steps/file_handling/file_handling.py
+++ First-steps/file_handling/file_handling.py
@@ -186,4 +186,32 @@ print("------------------")
 f = open("demofile3.txt", "r")
 print(f.read())
 
-print("------------------")
+# Note: the "w" method will overwrite the entire file.
+
+print("----------------------create new file------------------------------")
+
+"""
+Create a New File
+To create a new file in Python, use the open() method, with one of the following parameters:
+
+"x" - Create - will create a file, returns an error if the file exist
+
+"a" - Append - will create a file if the specified file does not exist
+
+"w" - Write - will create a file if the specified file does not exist
+
+Example
+Create a file called "myfile.txt":
+"""
+
+f = open("myfile.txt", "x")
+
+# Result: a new empty file is created!
+
+"""
+Example
+Create a new file if it does not exist:
+"""
+
+f = open("myfile.txt", "w")
+
