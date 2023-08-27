7e1215cdc1f1d9082e0333a14d74fff52cc48e76
Nagy Zsolt <zsolt.nagy.1976@gmail.com>
2023-08-27 17:13:33 +0200

    learn py

--- First-steps/file_handling/file_handling.py
+++ First-steps/file_handling/file_handling.py
@@ -140,4 +140,35 @@ f.close()
 # Note: You should always close your files, in some cases, due to buffering,
 # changes made to a file may not show until you close the file.
 
+print("-----------------------------wirte file-------------------------")
 
+"""
+Python File Write
+Write to an Existing File
+To write to an existing file, you must add a parameter to the open() function:
+
+"a" - Append - will append to the end of the file
+
+"w" - Write - will overwrite any existing content
+
+ExampleGet your own Python Server
+Open the file "demofile2.txt" and append content to the file:
+
+f = open("demofile2.txt", "a")
+f.write("Now the file has more content!")
+f.close()
+
+#open and read the file after the appending:
+f = open("demofile2.txt", "r")
+print(f.read())
+Example
+Open the file "demofile3.txt" and overwrite the content:
+
+f = open("demofile3.txt", "w")
+f.write("Woops! I have deleted the content!")
+f.close()
+
+#open and read the file after the overwriting:
+f = open("demofile3.txt", "r")
+print(f.read())
+"""
