f7aecdb3a195ed04592180ea46c5a001f4eefa08
Nagy Zsolt <zsolt.nagy.1976@gmail.com>
2023-08-27 17:16:17 +0200

    learn py

--- First-steps/file_handling/file_handling.py
+++ First-steps/file_handling/file_handling.py
@@ -151,24 +151,34 @@ To write to an existing file, you must add a parameter to the open() function:
 
 "w" - Write - will overwrite any existing content
 
-ExampleGet your own Python Server
+Example
 Open the file "demofile2.txt" and append content to the file:
+"""
 
 f = open("demofile2.txt", "a")
 f.write("Now the file has more content!")
 f.close()
 
+print("------------------")
+
 #open and read the file after the appending:
 f = open("demofile2.txt", "r")
 print(f.read())
+
+"""
 Example
 Open the file "demofile3.txt" and overwrite the content:
+
+"""
 
 f = open("demofile3.txt", "w")
 f.write("Woops! I have deleted the content!")
 f.close()
 
+print("------------------")
+
 #open and read the file after the overwriting:
 f = open("demofile3.txt", "r")
 print(f.read())
-"""
+
+print("------------------")
