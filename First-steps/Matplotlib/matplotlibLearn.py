43c8ac36a4bb3010aed1dd337d03293e04b57b98
Nagy Zsolt <zsolt.nagy.1976@gmail.com>
2023-08-30 00:04:10 +0200

    learn py

--- First-steps/Matplotlib/matplotlibLearn.py
+++ First-steps/Matplotlib/matplotlibLearn.py
@@ -13,4 +13,25 @@ import matplotlib
 
 print(matplotlib.__version__)
 
+print("---------------------------------Pyplot----------------------------------")
 
+"""
+Matplotlib Pyplot
+Pyplot
+Most of the Matplotlib utilities lies under the pyplot submodule, and are usually imported under the plt alias:
+
+import matplotlib.pyplot as plt
+Now the Pyplot package can be referred to as plt.
+
+ExampleGet your own Python Server
+Draw a line in a diagram from position (0,0) to position (6,250):
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+
+plt.plot(xpoints, ypoints)
+plt.show()
+"""
