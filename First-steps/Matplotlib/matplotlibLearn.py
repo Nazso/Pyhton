f11e3b64c1f9454db498915cbada341389aacd64
Nagy Zsolt <zsolt.nagy.1976@gmail.com>
2023-08-30 00:11:44 +0200

    learn py

--- First-steps/Matplotlib/matplotlibLearn.py
+++ First-steps/Matplotlib/matplotlibLearn.py
@@ -55,8 +55,28 @@ Example
 Draw a line in a diagram from position (1, 3) to position (8, 10):
 """
 
+# xpoints = np.array([1, 8])
+# ypoints = np.array([3, 10])
+
+# plt.plot(xpoints, ypoints)
+# plt.show()
+
+"""
+The x-axis is the horizontal axis.
+
+The y-axis is the vertical axis.
+"""
+
+"""
+Plotting Without Line
+To plot only the markers, you can use shortcut string notation parameter 'o', which means 'rings'.
+
+Example
+Draw two points in the diagram, one at position (1, 3) and one in position (8, 10):
+"""
+
 xpoints = np.array([1, 8])
 ypoints = np.array([3, 10])
 
-plt.plot(xpoints, ypoints)
+plt.plot(xpoints, ypoints, 'o')
 plt.show()
