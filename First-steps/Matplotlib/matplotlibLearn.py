36bc2bfd98dcf6818df99dd59ade217becd8398b
Nagy Zsolt <zsolt.nagy.1976@gmail.com>
2023-08-30 00:14:22 +0200

    learn py

--- First-steps/Matplotlib/matplotlibLearn.py
+++ First-steps/Matplotlib/matplotlibLearn.py
@@ -75,10 +75,23 @@ Example
 Draw two points in the diagram, one at position (1, 3) and one in position (8, 10):
 """
 
-xpoints = np.array([4, 8])
-ypoints = np.array([4, 10])
+# xpoints = np.array([4, 8])
+# ypoints = np.array([4, 10])
 
-plt.plot(xpoints, ypoints, 'o')
+# plt.plot(xpoints, ypoints, 'o')
+# plt.show()
+
+"""
+Multiple Points
+You can plot as many points as you like, just make sure you have the same number of points in both axis.
+
+Example
+Draw a line in a diagram from position (1, 3) to (2, 8) then to (6, 1) and finally to position (8, 10):
+"""
+
+xpoints = np.array([1, 2, 6, 8])
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(xpoints, ypoints)
 plt.show()
-
 
