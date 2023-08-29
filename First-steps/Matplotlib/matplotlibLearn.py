7ca2b58d10b938c4a1438172a95133071e27b865
Nagy Zsolt <zsolt.nagy.1976@gmail.com>
2023-08-30 00:15:25 +0200

    learn py

--- First-steps/Matplotlib/matplotlibLearn.py
+++ First-steps/Matplotlib/matplotlibLearn.py
@@ -89,9 +89,25 @@ Example
 Draw a line in a diagram from position (1, 3) to (2, 8) then to (6, 1) and finally to position (8, 10):
 """
 
-xpoints = np.array([1, 2, 6, 8])
-ypoints = np.array([3, 8, 1, 10])
+# xpoints = np.array([1, 2, 6, 8])
+# ypoints = np.array([3, 8, 1, 10])
 
-plt.plot(xpoints, ypoints)
+# plt.plot(xpoints, ypoints)
+# plt.show()
+
+
+"""
+Default X-Points
+If we do not specify the points on the x-axis, they will get the default values 0, 1, 2, 3 etc., depending on the length of the y-points.
+
+So, if we take the same example as above, and leave out the x-points, the diagram will look like this:
+
+Example
+Plotting without x-points:
+"""
+
+ypoints = np.array([3, 8, 1, 10, 5, 7])
+
+plt.plot(ypoints)
 plt.show()
 
