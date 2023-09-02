b467da899557b3c32b63edf7e6c8ecf23624b2c8
Nagy Zsolt <zsolt.nagy.1976@gmail.com>
2023-09-02 23:09:04 +0200

    learn py

--- First-steps/Matplotlib/matplotlibLearn.py
+++ First-steps/Matplotlib/matplotlibLearn.py
@@ -131,5 +131,39 @@ Mark each point with a circle:
 ypoints = np.array([3, 8, 1, 10])
 
 # plt.plot(ypoints, marker = 'o')
-plt.plot(ypoints, marker = '*')
+# plt.plot(ypoints, marker = '*')
+# plt.plot(ypoints, marker = 's')
+# plt.plot(ypoints, marker = '4')
+plt.plot(ypoints, marker = 'D')
 plt.show()
+
+"""
+Marker Reference
+You can choose any of these markers:
+
+Marker	Description
+'o'	Circle	
+'*'	Star	
+'.'	Point	
+','	Pixel	
+'x'	X	
+'X'	X (filled)	
+'+'	Plus	
+'P'	Plus (filled)	
+'s'	Square	
+'D'	Diamond	
+'d'	Diamond (thin)	
+'p'	Pentagon	
+'H'	Hexagon	
+'h'	Hexagon	
+'v'	Triangle Down	
+'^'	Triangle Up	
+'<'	Triangle Left	
+'>'	Triangle Right	
+'1'	Tri Down	
+'2'	Tri Up	
+'3'	Tri Left	
+'4'	Tri Right	
+'|'	Vline	
+'_'	Hline
+"""
