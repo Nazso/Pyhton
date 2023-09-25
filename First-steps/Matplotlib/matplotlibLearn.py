86d4eb1c6ef15a1a7b19c9e76d9cb953d4baa876
Nagy Zsolt <zsolt.nagy.1976@gmail.com>
2023-09-26 01:02:00 +0200

    learn py

--- First-steps/Matplotlib/matplotlibLearn.py
+++ First-steps/Matplotlib/matplotlibLearn.py
@@ -335,10 +335,29 @@ Example
 Draw two lines by specifying a plt.plot() function for each line:
 """
 
+# y1 = np.array([3, 8, 1, 10])
+# y2 = np.array([6, 2, 7, 11])
+
+# plt.plot(y1)
+# plt.plot(y2)
+
+# plt.show()
+
+"""
+You can also plot many lines by adding the points for the x- and y-axis for each line in the same plt.plot() function.
+
+(In the examples above we only specified the points on the y-axis, meaning that the points on the x-axis got the the default values (0, 1, 2, 3).)
+
+The x- and y- values come in pairs:
+
+Example
+Draw two lines by specifiyng the x- and y-point values for both lines:
+"""
+
+x1 = np.array([0, 1, 2, 3])
 y1 = np.array([3, 8, 1, 10])
+x2 = np.array([0, 1, 2, 3])
 y2 = np.array([6, 2, 7, 11])
 
-plt.plot(y1)
-plt.plot(y2)
-
+plt.plot(x1, y1, x2, y2)
 plt.show()
